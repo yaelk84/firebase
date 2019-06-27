c3e4077b5d551fd8779c10c2f45762fae474bb39
yaelk <yaelk8484@gmail.com>
2019-06-27 15:06:10 +0300

    branch

--- poalim/poalim-branches/src/app/features/branch-list/branch-list.component.ts
+++ poalim/poalim-branches/src/app/features/branch-list/branch-list.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { Observable } from 'rxjs';
-import { BRANCHESDATA } from '../../data/mock-branches';
+import {Component, OnInit, Input} from '@angular/core';
+import {BRANCHESDATA} from '../../data/mock-branches';
 import {BranchBox} from "../../core/interface/branch-box";
 import {BranchObj} from "../../core/models/branch-model";
 import {BranchDataService} from "../../core/services/branch-data.service";
+import {ApiService} from '../../core/services/api.service';
+import {logger} from "codelyzer/util/logger";
+import {Observable, of, Subject, Subscription} from 'rxjs';
+import {catchError, map, mergeMap} from 'rxjs/operators';
 
 
 @Component({
@@ -12,27 +15,37 @@ import {BranchDataService} from "../../core/services/branch-data.service";
   styleUrls: ['./branch-list.component.sass']
 })
 export class BranchListComponent implements OnInit {
-  constructor(private branchDataServices:BranchDataService) { }
+  constructor(private branchDataServices: BranchDataService,private apiService:ApiService) {
+  }
+
   data;
   observableData;
-  branchNewArray:BranchObj[]=[];
-  branchFetched:BranchObj;
+  branchNewArray: BranchObj[] = [];
+  branchFetched: BranchObj;
+
   ngOnInit() {
     /** replace with http after**/
-    this.data=BRANCHESDATA;
+    this.data = BRANCHESDATA;
     this.observableData = Observable.create((observer) => {
-       observer.next( this.data[0]);
-      observer.next( this.data[1]);
+      observer.next(this.data[0]);
+      observer.next(this.data[1]);
       observer.complete();
     });
+debugger ;
+    return this.apiService.getRoleDetails().subscribe((val) => {
+      debugger
+      console.log("response", val);
+      const branchFetched = this.branchDataServices.createSingleBranch(val);
+      this.branchNewArray.push(new BranchObj(branchFetched.id, branchFetched.branchNum, branchFetched.branchName, branchFetched.address, branchFetched.distance, branchFetched.openHours, branchFetched.closeHours, branchFetched.closeNow, branchFetched.change));
+    });
     /** end replace with http after**/
-    this.observableData.subscribe(val =>{
-      debugger;
-      const branchFetched=this.branchDataServices.createSingleBranch(val);
-      this.branchNewArray.push(new BranchObj(branchFetched.id,branchFetched.branchNum,branchFetched.branchName,branchFetched.address,branchFetched.distance,branchFetched.openHours,branchFetched.closeHours,branchFetched.closeNow,branchFetched.change));
-  }
-    );
-    console.log(this.branchNewArray,"branchNewArray");
+  /*  this.observableData.subscribe((val) => {
+
+
+      }, (err) => {
+          console.log('err', err);
+      }
+    );*/
   }
 
 }
