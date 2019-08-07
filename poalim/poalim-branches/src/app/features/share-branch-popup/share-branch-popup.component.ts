db635419405ba9cae4bf907f5c7c95b23d5e480b
Amit Barletz <amitb@rcdc.local>
2019-08-07 10:23:24 +0300

    popup mailto and copy link

--- poalim/poalim-branches/src/app/features/share-branch-popup/share-branch-popup.component.ts
+++ poalim/poalim-branches/src/app/features/share-branch-popup/share-branch-popup.component.ts
@@ -14,4 +14,19 @@ export class ShareBranchPopupComponent implements OnInit {
   ngOnInit() {
   }
 
+  copyLink(item) {
+    item = window.location.href;
+    document.addEventListener('copy', (e: ClipboardEvent) => {
+      e.clipboardData.setData('text/plain', (item));
+      e.preventDefault();
+      document.removeEventListener('copy', null);
+    });
+    document.execCommand('copy');
+    // const branchLinkToShare = window.location.href;
+    // console.log('link to share !@#$%^&*', branchLinkToShare);
+  }
+  ShareBranchLinkByMail() {
+    const clientMail = 'mailto:address@dmail.com';
+    window.location.href = clientMail;
+  }
 }
