ee8ac2c09c4e2849874b7dd6f1a45d3cbe983ab4
jeninemir <jenin@realcommerce.co.il>
2019-12-18 15:27:52 +0200

    update enviroment

--- poalim/poalim-branches/src/environments/environment.prod.ts
+++ poalim/poalim-branches/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: '/api/branches/',
+  apiBaseUrl: '/he/api/branches/',
   translationPath: '/sites/all/themes/poalim/js/angular/assets/translate/',
   configPath: '/sites/all/themes/poalim/js/angular/assets/config.json',
   imgUrlPath: '/sites/all/themes/poalim/js/angular/assets/media/',
